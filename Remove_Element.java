6dd6fc586ecf8e001b56de9144e4229eaad9beb5
Youchen <youchen.r@gmail.com>
2015-01-04 16:51:10 -0500

    O(n) 440 ms Array
    v1.0

--- Remove_Element.java
+++ Remove_Element.java
@@ -8,19 +8,19 @@ import java.util.*;
 public class Solution {
     public int removeElement(int[] A, int elem) {
     	//Solution #1 529 ms
-//         ArrayList<Integer> arr = new ArrayList<Integer>();
+        ArrayList<Integer> arr = new ArrayList<Integer>();
         
-// 		for(int item: A) {
-// 			arr.add((Integer)item);
-// 		}
+		for(int item: A) {
+			arr.add((Integer)item);
+		}
 		
-// // 		while(arr.contains(new Integer(elem)))
-// 		while(arr.remove(new Integer(elem)));
+// 		while(arr.contains(new Integer(elem)))
+		while(arr.remove(new Integer(elem)));
 		
-// 		for(int i = 0; i < arr.size(); i++)
-// 		    A[i] = arr.get(i);
+		for(int i = 0; i < arr.size(); i++)
+		    A[i] = arr.get(i);
 		    
-// 		return arr.size();
+		return arr.size();
 
 		//Solution #2 440 ms
 		//Copied from https://oj.leetcode.com/discuss/3753/my-solution-for-your-reference
@@ -31,13 +31,13 @@ public class Solution {
 		// return begin;
 
 		//Solution #3 440 ms
-		int size = 0;
-		for (int i = 0; i < A.length; ++i) {
-			if (A[i] != elem) {
-				A[size] = A[i];
-				size++;
-			}
-		}
-		return size;
+		// int size = 0;
+		// for (int i = 0; i < A.length; ++i) {
+		// 	if (A[i] != elem) {
+		// 		A[size] = A[i];
+		// 		size++;
+		// 	}
+		// }
+		// return size;
 	}//removeElement
 }
