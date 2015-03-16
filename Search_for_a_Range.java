421cf5bb03ce38d69ab168ec176df68264d0a9a5
Youchen <youchen.r@gmail.com>
2015-03-16 18:07:24 -0400

    O(log n)  O(1)  271 ms BS
    v1.0

--- Search_for_a_Range.java
+++ Search_for_a_Range.java
@@ -5,32 +5,35 @@
  * 
  * Note:
  *      Need to master the BS application for real problem.
+ * 
+ * Reference:
+ *      https://leetcode.com/discuss/19368/very-simple-java-solution-with-only-binary-search-algorithm
  */
 public class Solution {
     public int[] searchRange(int[] A, int target) {
-    	if(A.length == 0 || A == null)
-    		return null;
-    	int[] range = {-1, -1};
-    	int start = firstGreaterEqual(A, target);
-    	if(start == A.length || A[start] != target)
-    		return range;
+        if(A.length == 0 || A == null)
+            return null;
+        int[] range = {-1, -1};
+        int start = firstGreaterEqual(A, target);
+        if(start == A.length || A[start] != target)
+            return range;
 
-    	//found target
-    	int end = firstGreaterEqual(A, target + 1) - 1;
-    	range[0] = start;
-    	range[1] = end;
-    	return range;
+        //found target
+        int end = firstGreaterEqual(A, target + 1) - 1;
+        range[0] = start;
+        range[1] = end;
+        return range;
     }
 
     public int firstGreaterEqual(int[] A, int target){
-    	int left = 0, right = A.length;
-    	while(left < right){
-    		int mid = left + (right - left) / 2;
-    		if(A[mid] < target)
-    			left = mid + 1;
-    		else
-    			right = mid;
-    	}
-    	return left;
+        int left = 0, right = A.length;
+        while(left < right){
+            int mid = left + (right - left) / 2;
+            if(A[mid] < target)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
     }
 }
